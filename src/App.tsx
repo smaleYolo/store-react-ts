54b57fa1f4ea05134dbc78df2bf17335f4c2c38e
smaleYolo <ilia.artamonov@yandex.ru>
2023-02-07 12:05:31 +0300

    Finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import {Routes, Route} from "react-router-dom";
-import Home from "./pages/Home";
 import Store from "./pages/Store";
 import About from "./pages/About";
 import Navbar from "./components/Navbar";
@@ -14,6 +13,7 @@ function App() {
             <Container className='mb-4'>
                 <Routes>
                     <Route path='/' element={<Store/>}/>
+                    <Route path='*' element={<Store/>}/>
                     <Route path='/about' element={<About/>}/>
                 </Routes>
             </Container>
